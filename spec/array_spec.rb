e3b6116e6e7b284fb0fd4ae8d69359ba618b39d6
katebeavis <katebeavis84@gmail.com>
2015-04-27 18:48:14 +0100

    Added two more tests

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -16,4 +16,14 @@ describe Array do
   it 'can add numbers when accumulator is changed' do
     expect([1, 2, 3, 4].injection_replicate(10) { |result, element| result + element }).to eq(20)
   end
+
+  it 'does not destroy the original block given' do
+    block = [1, 2, 3, 4]
+    block.injection_replicate { |result, element| result + element }
+    expect(block).to eq([1, 2, 3, 4])
+  end
+
+  it 'can join words to make a string' do
+    expect(['I ', 'am ', 'a ', 'maker ', 'who ', 'codes'].injection_replicate { |result, element| result << element }).to eq('I am a maker who codes')
+  end
 end
